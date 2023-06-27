a0e3b895fdd8cc77eb72ff4336f16e5a0f25d791
Paloma Cardoso <42698693+palomacardoso@users.noreply.github.com>
2023-06-27 19:52:50 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,74 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground; //chão
+var left; //parede da esquerda
+var right;  //parede da direita
+var top_wall;  //teto
+var ball; //bolinha
+
+var btn1; //botao vermelho - direita
+var btn2; //botao verde - cima 
+
+function setup() 
+{
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  btn1 = createImg('right.png');
+  btn1.position(220,30);
+  btn1.size(50,50);
+  btn1.mouseClicked(hForce);
+  
+  btn2 = createImg('up.png');
+  btn2.position(20,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+  
+  //aqui criamos os objetos
+  ground = new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+
+  var ball_options = {
+    restitution: 0.95
+  }
+
+  ball = Bodies.circle(200,100,20,ball_options);
+  World.add(world,ball);
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(51);
+
+  ellipse(ball.position.x,ball.position.y,20);
+
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  Engine.update(engine) ;
+}
+
+
+function hForce() //força na horizontal
+{
+  //aplicando uma força para direita na bolinha 
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+}
+
+function vForce() // força na vertical
+{
+  //aplicando uma força para cima na bolinha 
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}
